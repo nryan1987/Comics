017e0f7f1ddc172f9dab0c4c5256316a4178e295
Ryan <nryan.brannan@gmail.com>
2019-07-27 13:52:33 -0500

    Add volume number to query.

--- missingIssues.php
+++ missingIssues.php
@@ -33,7 +33,7 @@ extract($row);
 	//Add the aliases to the missing issues table.
 	$sql="SELECT DISTINCT Comics.Title as qryTitle, Comics.Issue, Comics.Volume as qryVolume, ComicAlias.Title as aliasTitle, ComicAlias.Issue as aliasIssue, ComicAlias.Volume as aliasVolume
 	from ComicAlias inner join Comics on Comics.ComicID=ComicAlias.ComicID
-	where ComicAlias.Title=\"$Title\"";
+	where ComicAlias.Title=\"$Title\" AND (Comics.Volume=$Volume or ComicAlias.Volume=$Volume)";
 	$result=mysqli_query($cxn,$sql) or die("Could not add aliases to missing issues table.<br>SQL ERROR: ".mysqli_error($cxn)."<br>".$sql);
 	while($row=mysqli_fetch_assoc($result)) {
 		extract($row);
